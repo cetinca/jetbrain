988269e63e71cb4b85783099e25a2f3daec924d8
Cetin CAKIR <61231974+cetinca@users.noreply.github.com>
2022-05-01 10:33:07 +0300

    Daily commit

---
+++ JavaScript/currency_converter/app.js
@@ -0,0 +1,11 @@
+// npm install prompt-sync
+// node app.js
+// const input = require('sync-input');
+const input = require('prompt-sync')();
+
+console.log(`Welcome to Currency Converter!
+1 USD equals  1 USD
+1 USD equals  113.5 JPY
+1 USD equals  0.89 EUR
+1 USD equals  74.36 RUB
+1 USD equals  0.75 GBP`)
